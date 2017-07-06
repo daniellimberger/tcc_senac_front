d37c4a75834b3928c24367b303e6ffd1fa9ad251
daniellimberger <daniellimberger@hotmail.com>
2017-07-06 01:41:01 -0300

    tela de produto - sequencia no CRUD (insert)

--- app/src/produto/produto_controller.js
+++ app/src/produto/produto_controller.js
@@ -1,9 +1,57 @@
-myApp.controller('produtoController',  ['$scope', '$rootScope',  function($scope, $rootScope){
+myApp.controller('produtoController',  ['$scope', '$http','$rootScope',  function($scope, $http, $rootScope){
 
 	$rootScope.tituloPagina = "Produto";
 
-	$scope.produto = "";
-	$scope.marca = "";
-	$scope.valor="";
+	$scope.buscarDadosProduto = function(){
+		$http
+		.get("http://localhost:8081/Controller/produto_controller.php?function=listar_todos")
+		.then(function(data){
+				$scope.produtos = data;
+				console.log($scope.produtos);
+		});
+	}
 
+	$scope.buscarDadosProduto();
+
+
+
+
+	$scope.produto = [];
+
+	$scope.cadastraProduto = function(){
+		return $http({
+					  method  : 'POST',
+					  url     :  "http://localhost:8081/Controller/produto_controller.php?function=cadastrar",
+					  data: {
+					  		nome : $scope.produto.nome,
+					  		marca: $scope.produto.marca,
+					  		valor: $scope.produto.valor
+					        },
+					  headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
+					 });
+					  console.log('obaaaaa');		
+	}
+
+
+	$scope.lala = function(){
+		/*$scope.produto.nome = dados.nome;
+		$scope.produto.marca = dados.marca;
+		$scope.produto.valor = dados.valor;*/
+		//$scope.depreciacao.codigo        = dados.codigo;
+
+
+		$http({
+		method  : 'POST',
+		url     :  "http://localhost:8081/Controller/produto_controller.php?function=cadastrar",
+		data: {
+		    	nome        : $scope.produto.nome,
+		    	marca       : $scope.produto.marca,
+		    	valor       : $scope.produto.valor
+		    },
+		headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)		
+		});
+
+
+	}
+	
 }]);
