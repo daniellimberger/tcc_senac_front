260b05b35de4b0effe9d3a78ae622f293661d794
daniellimberger <daniellimberger@hotmail.com>
2017-08-06 23:45:18 -0300

    CREATE, RETRIEVE e DELETE para as telas de Clientes, Vendedores e Produtos

--- app/src/cliente/cliente_controller.js
+++ app/src/cliente/cliente_controller.js
@@ -1,18 +1,148 @@
-myApp.controller('clienteController',  ['$scope', '$rootScope', function($scope, $rootScope){
+myApp.controller('clienteController',  ['$scope', '$http','$rootScope',  function($scope, $http, $rootScope, $mdDateLocale){
+
+
 
 	$rootScope.tituloPagina = "Cadastro de Cliente";
 
-	$scope.nomeFantasia="";
-	$scope.razaoSocial="";
-	$scope.cnpj="";
-	$scope.inscEstadual="";
-	$scope.endereco="";
-	$scope.bairro="";
-	$scope.cidade="";
-	$scope.cep="";
-	$scope.uf="";
-	$scope.dataCadastro="";
-	$scope.observacao="";
+	$rootScope.mostrarBtnCad = true;			
+
+	$scope.buscarDadosCliente = function(){
+		$http
+		.get(url_base+"Controller/cliente_controller.php?function=listar_todos")
+		.then(function(data){
+				$scope.clientes = data;
+		});
+	}
+	$scope.buscarDadosCliente();
+
+
+
+	$scope.cliente = [];
+	$scope.opas = [];
+
+
+	$scope.cadastraCliente = function(){
+					$http({
+					  method  : 'POST',
+					  url     :  url_base+"Controller/cliente_controller.php?function=cadastrar",
+					  data: {
+					  		nomeFantasia : $scope.cliente.nomeFantasia,
+					  		  razaoSocial: $scope.cliente.razaoSocial,
+					  		         cnpj: $scope.cliente.cnpj,
+				  		     inscEstadual: $scope.cliente.inscEstadual,
+				  		         endereco: $scope.cliente.endereco,
+					  		       bairro: $scope.cliente.bairro,
+					  		       cidade: $scope.cliente.cidade,
+				  		         	  cep: $scope.cliente.cep,
+					  		           uf: $scope.cliente.uf,
+			  		         telefoneFixo: $scope.cliente.telefoneFixo,
+			  		         dataCadastro: moment($scope.cliente.dataCadastro).format('YYYY-MM-DD'),
+		 		    dataCadastroExtendida: $scope.cliente.dataCadastro,
+		 		               observacao: $scope.cliente.observacao
+
+					        },
+					  headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
+					 }).then(function (data) {
+    					//console.log(moment($scope.cliente.dataCadastro).format('YYYY-MM-DD'));
+    					alert( moment($scope.cliente.dataCadastro).format('YYYY-MM-DD'));
+    					alert( $scope.cliente.dataCadastro );
+    					console.log(data);
+					 });
+
+		$scope.buscarDadosCliente();		
+
+		$scope.nomeFantasia="";
+		$scope.razaoSocial="";
+		$scope.cnpj="";
+		$scope.inscEstadual="";
+		$scope.endereco="";
+		$scope.bairro="";
+		$scope.cidade="";
+		$scope.cep="";
+		$scope.uf="";
+		$scope.telefoneFixo="";
+		$scope.dataCadastro="";	
+		$scope.dataCadastroExtendida="";			
+		$scope.observacao="";
+
+	}
+
+	$scope.deletaCliente = function(id_deletar){
+
+
+		$http({
+		method  : 'POST',
+		url     :  url_base+"Controller/cliente_controller.php?function=deletar",
+		data: {
+		    	id_deletar        : id_deletar,
+		    },
+		headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)		
+		}).then(function (data) {
+			alert('deletou');
+      	});
+
+		$scope.buscarDadosCliente();
+		// atualiza a lista
+		// poderia usar watch
+
+	}	
+
+	$scope.editaCliente = function(id_editar){
+
+alert(id_editar);
+console.log(id_editar);
+/*		$http({
+		method  : 'POST',
+		url     :  url_base+"Controller/cliente_controller.php?function=deletar",
+		data: {
+		    	id_deletar        : id_deletar,
+		    },
+		headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)		
+		}).then(function (data) {
+			alert('deletou');
+      	});
+
+		$scope.buscarDadosCliente();
+		// atualiza a lista
+		// poderia usar watch
+*/
+	}		
+
+	$scope.listarUm = function(id_listar){
+
+		$http({
+			url     :  url_base+"Controller/cliente_controller.php?function=listar_um&id_listar="+id_listar,
+			dataType: 'json',
+		}).then(function (retorno) {
+
+			opa = new Date(retorno.data.data_cadastro);
+			opa.setDate(opa.getDate() + 1);
+
+			$scope.cliente.id_cliente = retorno.data.id_cliente;
+			$scope.cliente.nomeFantasia = retorno.data.nome_fantasia;
+			$scope.cliente.razaoSocial = retorno.data.razao_social;
+			$scope.cliente.cnpj = retorno.data.cnpj;
+			$scope.cliente.inscEstadual = retorno.data.insc_estadual;
+			$scope.cliente.endereco = retorno.data.endereco;
+			$scope.cliente.bairro = retorno.data.bairro;
+			$scope.cliente.cidade = retorno.data.cidade;
+			$scope.cliente.cep = retorno.data.cep;
+			$scope.cliente.uf = retorno.data.uf;
+			$scope.cliente.telefoneFixo = retorno.data.telefone_fixo;
+			$scope.cliente.dataCadastro = opa;
+			$scope.cliente.observacao = retorno.data.observacao;
+
+			$rootScope.mostrarBtnCad = false;
+			$rootScope.mostrarBtnEdit = true;			
+
+      	});
+
+		//$scope.buscarDadosCliente();
+		// atualiza a lista
+		// poderia usar watch
+	}	
+
+
 
    
 }]);
--- app/src/vendedor/vendedor_controller.js
+++ app/src/vendedor/vendedor_controller.js
@@ -1,19 +1,106 @@
-myApp.controller('vendedorController',  ['$scope', '$rootScope',  function($scope, $rootScope){
+myApp.controller('vendedorController',  ['$scope', '$http','$rootScope',  function($scope, $http, $rootScope, $mdDateLocale){
+
+
 
 	$rootScope.tituloPagina = "Cadastro de Vendedor";
 
-	$scope.nome="";
-	$scope.sobrenome="";
-	$scope.telefoneFixo="";	
-	$scope.telefoneCelular="";
-	$scope.rg="";
-	$scope.cpf="";
-	$scope.endereco="";
-	$scope.bairro="";
-	$scope.cidade="";
-	$scope.cep="";
-	$scope.uf="";
-	$scope.dataCadastro="";
-	$scope.observacao="";
+	$scope.buscarDadosVendedor = function(){
+		$http
+		.get(url_base+"Controller/vendedor_controller.php?function=listar_todos")
+		.then(function(data){
+				$scope.vendedores = data;
+		});
+	}
+	$scope.buscarDadosVendedor();
+
+
+
+//	$scope.vendedor = [];
+
+	$scope.vendedor = ({
+						      nome: 'Ligiano', 
+						 sobrenome: 'Azzi',
+						        rg: '00.000.000/0001-10',
+				               cpf: '007.836.980-03',
+                      telefoneFixo: '51 3468-9000',				               
+                   telefoneCelular: '51 9305.6977',	                    
+			              endereco: 'Rua Sete Povos, 78',
+				            bairro: 'Nossa Sra. das Graças',
+				            cidade: 'Canoas',
+				               cep: '94920-170',
+				                uf: 'RS',
+                      dataCadastro: new Date(),
+	                    observacao: 'nada a declarar'
+					  });
+
+
+
+	$scope.cadastraVendedor = function(){
+					$http({
+					  method  : 'POST',
+					  url     :  url_base+"Controller/vendedor_controller.php?function=cadastrar",
+					  data: {
+					  		      nome : $scope.vendedor.nome,
+					  		  sobrenome: $scope.vendedor.sobrenome,
+					  		         rg: $scope.vendedor.rg,
+				  		            cpf: $scope.vendedor.cpf,
+				           telefoneFixo: $scope.vendedor.telefoneFixo,
+				        telefoneCelular: $scope.vendedor.telefoneCelular,
+					  	       endereco: $scope.vendedor.endereco,
+				  		     	 bairro: $scope.vendedor.bairro,
+					  		     cidade: $scope.vendedor.cidade,
+			  		                cep: $scope.vendedor.cep,
+				  		             uf: $scope.vendedor.uf,			  		                
+			  		       dataCadastro: moment($scope.vendedor.dataCadastro).format('YYYY-MM-DD'),
+ 		  		       	     observacao: $scope.vendedor.observacao
+					        },
+					  headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
+					 }).then(function (data) {
+    					console.log(data);
+					 });
+
+		$scope.buscarDadosVendedor();		
+
+		// limpa os campos do formulário
+		$scope.nome="";
+		$scope.sobrenome="";
+		$scope.rg="";
+		$scope.cpf="";
+		$scope.telefoneFixo="";
+		$scope.telefoneCelular="";
+		$scope.endereco="";
+		$scope.bairro="";
+		$scope.cidade="";
+		$scope.cep="";
+		$scope.dataCadastro="";	
+		$scope.observacao="";
+
+	}
+
+	$scope.deletaVendedor = function(id_deletar){
+
+		$http({
+		method  : 'POST',
+		url     :  url_base+"Controller/vendedor_controller.php?function=deletar",
+		data: {
+		    	id_deletar        : id_deletar,
+		    },
+		headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)		
+		}).then(function (data) {
+			alert('deletou');
+      	});
+
+		$scope.buscarDadosVendedor();
+		// atualiza a lista
+		// poderia usar watch
+
+	}	
+
+
+	$scope.opa = function (opa){
+		alert(opa);
+	}
+
+
    
 }]);
